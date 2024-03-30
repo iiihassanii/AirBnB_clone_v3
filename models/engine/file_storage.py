79adc175e4181b0a08b4d94850998cff6c711345
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-30 16:14:46 +0300

    task2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -70,14 +70,21 @@ class FileStorage:
         self.reload()
 
     def get(self, cls, id):
-        '''object to get'''
-        if cls and id:
-            takeObj = '{}.{}'.format(cls, id)
-            everyObj = self.all(cls)
-            return everyObj.get(takeObj)
+        """
+        cls: class
+        id: string representing the object ID
+        """
+        if cls is not None and id is not None and cls in classes.values():
+            key = cls.__name__ + '.' + id
+            return self.__objects.get(key, None)
         else:
             return None
 
     def count(self, cls=None):
-        '''class that is (optional)'''
-        return (len(self.all(cls)))
+        """cls: class (optional)"""
+        total = 0
+        if cls is not None and cls in classes.values():
+            total = len(self.all(cls))
+        if cls is None:
+            total = len(self.__objects)
+        return total
