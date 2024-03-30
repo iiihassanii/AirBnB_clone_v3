92fae86ba56ff7b216fc0183188ad914f959521e
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-30 17:05:43 +0300

    3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,5 +2,6 @@
 """Initialize Blueprint views"""
 from api.v1.views.index import *
 from flask import Blueprint
+from api.v1.views.index import *
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
