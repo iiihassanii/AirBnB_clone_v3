c69384e50b5fefa760fd7f48612d1b57895ae064
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-30 22:29:57 +0300

    task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,33 @@
 """index.py to connect to API"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
 
 
-@app_views.route('/status', methods=['GET'])
+classes = {"amenities": Amenity,
+           "cities": City,
+           "places": Place,
+           "reviews": Review,
+           "states": State,
+           "users": User
+           }
+
+
+@app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """count stats"""
+    dict = {}
+    for key, value in classes.items():
+        dict[key] = storage.count(value)
+    return jsonify(dict)
