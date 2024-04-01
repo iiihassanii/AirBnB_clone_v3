862adfcba4ac6a22aa8cc56ca24ce30333c8efc6
iiihassanii <hassanabdelazim.max@gmail.com>
2024-04-01 17:39:49 +0300

    task 7

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -19,41 +19,45 @@ def get_city_state(state_id):
     return jsonify(cities)
 
 
-@app_views.route('/states/<city_id>', methods=['GET'])
+@app_views.route('/cities/<city_id>', methods=['GET'])
 def get_city_by_id(city_id):
     """Get city by id"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    return jsonify(city.to_dict()), 'OK'
+    return jsonify(city.to_dict())
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'])
-def delete_city(state_id):
+def delete_city(city_id):
     """Delete City"""
-    city = storage.get(City, state_id)
-    if city is None:
-        abort(404)
-    storage.delete(city)
-    storage.save()
-    return jsonify({}), 200
+    city = storage.get(City, city_id)
+    if city:
+        storage.delete(city)
+        storage.save()
+        return jsonify({}), 200
+    else:
+        return abort(404)
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """CREATE City"""
-    data_dict = request.get_json(silent=True)
+    if request.content_type != 'application/json':
+        return jsonify({"error": "Not a JSON"}), 400
     state = storage.get(State, state_id)
     if not state:
         return abort(404)
+    if not request.get_json(silent=True):
+        return jsonify({"error": "Not a JSON"}), 400
 
-    if not data_dict:
-        abort(400, description="Not a JSON")
+    data = request.get_json(silent=True)
+    if 'name' not in data:
+        return abort(400, 'Missing name')
+    data['state_id'] = state_id
 
-    if 'name' not in data_dict:
-        abort(400, description="Missing name")
-
-    city = City(**data_dict)
+    city = City(**data)
     city.save()
     return jsonify(city.to_dict()), 201
 
